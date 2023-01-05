923f42f08528b95d64f8ce691bebe15b73f92f87
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-01-05 23:14:08 +0300

    Create Search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'searchs/index'
+  get 'search/index'
   root 'posts#index'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
